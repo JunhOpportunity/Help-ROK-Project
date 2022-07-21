7a3aee94d7282e8997a2d2c3f42daeae10b8bba0
JunHo Kim <89464762+JunhOpportunity@users.noreply.github.com>
2022-07-21 15:59:45 +0900

    Project Mark up Project Mark up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">HELP R.O.K</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Main</Nav.Link>
+              <Nav.Link href="#memo">Memo</Nav.Link>
+              <Nav.Link href="#wish">Wish</Nav.Link>
+              <Nav.Link href="#qa">Q & A</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* 메인 페이지 */}
+      {/* 메모 페이지 */}
+      {/* 위로의 명언 모음 페이지 */}
+      {/* 질문(커뮤니케이션) 페아지 */}
+      {/* 신고와 상담 연결 페이지 */}
     </div>
   );
 }
---
+++ src/pages/main.js
@@ -0,0 +1,11 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function mainPage() {
+  // 나의 신고 타임라인
+  // 오늘 가장 많은 관심을 받은 신고 내용
+  // 나에게 도움이 될만한 명언
+  // 개발자 contact
+}
